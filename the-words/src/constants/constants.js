b80609c4e43699728f0d96d5c2c5ac5b50d078eb
milad88884 <interceptors8888@gmail.com>
2023-03-29 18:38:46 +0200

    setting ip

--- the-words/src/constants/constants.js
+++ the-words/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "http://16.170.167.40:3000"
 export const SEARCH_WORD_URL = BASE_URL + "/search-word"
 export const ADD_NEW_WORD_URL = BASE_URL + "/add-new-word"
 export const ADD_NEW_SYNONYM_URL = BASE_URL + "/add-new-synonym"
