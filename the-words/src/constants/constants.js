75ecfc0a56a86f5707f57f964d75bf196debb41b
milad88884 <interceptors8888@gmail.com>
2023-03-29 18:49:13 +0200

    asdasdasd

--- the-words/src/constants/constants.js
+++ the-words/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://ec2-16-170-167-40.eu-north-1.compute.amazonaws.com::3000"
+export const BASE_URL = "http://ec2-16-170-167-40.eu-north-1.compute.amazonaws.com:3000"
 export const SEARCH_WORD_URL = BASE_URL + "/search-word"
 export const ADD_NEW_WORD_URL = BASE_URL + "/add-new-word"
 export const ADD_NEW_SYNONYM_URL = BASE_URL + "/add-new-synonym"
