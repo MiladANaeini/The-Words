5d072961946713b774c3a00cc015f674710dac15
milad88884 <interceptors8888@gmail.com>
2023-03-30 15:56:34 +0200

    change base URL

--- the-words/src/constants/constants.js
+++ the-words/src/constants/constants.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = "http://localhost:3000" //base URL for local
-export const BASE_URL = "http://ec2-16-170-167-40.eu-north-1.compute.amazonaws.com:3001" //base URL for production
+export const BASE_URL = "http://ec2-16-170-167-40.eu-north-1.compute.amazonaws.com:3000" //base URL for production
 export const SEARCH_WORD_URL = BASE_URL + "/search-word"
 export const ADD_NEW_WORD_URL = BASE_URL + "/add-new-word"
 export const ADD_NEW_SYNONYM_URL = BASE_URL + "/add-new-synonym"
+
